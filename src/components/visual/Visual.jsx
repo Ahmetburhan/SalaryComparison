d8f1f1230d8f7e9e0ec3126d36348ab8a0baf3b0
Ahmet Koylan <Ahmetburhan@gmail.com>
2019-07-23 02:07:52 -0700

    initial

--- src/components/visual/Visual.jsx
+++ src/components/visual/Visual.jsx
@@ -6,29 +6,33 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
 
 class Visual extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   componentDidMount() {
     // Create chart instance
     let chart = am4core.create("chartdiv", am4charts.PieChart);
-
     // Add data
-    chart.data = [
-      {
-        amount: "Base Salary",
-        dollars: 155000
-      },
-      {
-        amount: "Sign up Bonus",
-        dollars: 4000
-      },
-      {
-        amount: "RSU",
-        dollars: 50000
-      },
-      {
-        amount: "Performance Bonus",
-        dollars: 31000
-      }
-    ];
+    chart.data = this.props.data;
+    console.log(chart.data);
+    // chart.data = [
+    //   {
+    //     amount: "Base Salary",
+    //     dollars: 155000
+    //   },
+    //   {
+    //     amount: "Sign up Bonus",
+    //     dollars: 4000
+    //   },
+    //   {
+    //     amount: "RSU",
+    //     dollars: 50000
+    //   },
+    //   {
+    //     amount: "Performance Bonus",
+    //     dollars: 31000
+    //   }
+    // ];
 
     // Add and configure Series
     let pieSeries = chart.series.push(new am4charts.PieSeries());
@@ -46,8 +50,10 @@ class Visual extends React.Component {
   render() {
     return (
       <div>
-        <div id="chartdiv" style={{ width: "100%", height: "500px" }} />;
-        <h1>TC: 240K</h1>
+        <div id={this.props.comp} style={{ width: "100%", height: "500px" }} />;
+        <h1>
+          {this.props.comp} TC: {this.props.total}
+        </h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Visual from "../src/components/visual/Visual";
+
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Container, Row, Col } from "reactstrap";
+const data1 = [
+  {
+    amount: "Base Salary",
+    dollars: 155000
+  },
+  {
+    amount: "Sign up Bonus",
+    dollars: 4000
+  },
+  {
+    amount: "RSU",
+    dollars: 50000
+  },
+  {
+    amount: "Performance Bonus",
+    dollars: 31000
+  }
+];
+const data2 = [
+  {
+    amount: "Base Salary",
+    dollars: 155000
+  }
+];
 function App() {
   return (
     <div className="App">
       <h1>Presentation for Nazeer Babu</h1>
-      <Visual />
-      <h2>Start editing to see some magic happen!</h2>
+      <Container>
+        <Row>
+          <Col auto>
+            <Visual data={data1} comp={"Intuit"} total="240K" />
+          </Col>
+          <Col auto>
+            <Visual data={data2} comp={"Cisco"} total="155K" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
